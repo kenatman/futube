6ffb157e383f813452b317038b2384a5f8ede66b
Yunsu <kenatman0723@gmail.com>
2021-07-13 11:24:33 +0900

    adding styles

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,5 +1,20 @@
 import React from "react";
+import styles from "./video_item.module.css";
 
-const VideoItem = (props) => <li>{props.video.snippet.title}</li>;
+const VideoItem = ({ video: { snippet } }) => (
+  <li className={styles.container}>
+    <div className={styles.video}>
+      <img
+        className={styles.thumbnail}
+        src={snippet.thumbnails.high.url}
+        alt="thumbnail"
+      />
+      <div className={styles.metadata}>
+        <p className={styles.title}>{snippet.title}</p>
+        <p className={styles.channel}>{snippet.channelTitle}</p>
+      </div>
+    </div>
+  </li>
+);
 
 export default VideoItem;
