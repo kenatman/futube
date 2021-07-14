00475ceb165924d1db054d71562a8ab5a7f30de3
Yunsu <kenatman0723@gmail.com>
2021-07-14 15:35:23 +0900

    8.10 비디오 선택 & 상세화면 : 새로운 state추가 : props 전달 익숙해지기

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 import VideoList from "./components/video_list/video_list";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
   useEffect(() => {
     youtube
       .mostPopular() //
@@ -16,10 +18,28 @@ function App({ youtube }) {
       .search(term) //
       .then((videos) => setVideos(videos));
   };
+
+  const handleSelect = (video) => {
+    setSelectedVideo(video);
+  };
+
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={handleSearch} />
-      <VideoList videos={videos} />;
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onSelect={handleSelect}
+            display={selectedVideo ? `list` : `grid`}
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import VideoItem from "../video_item/video_item";
 import styles from "./video_list.module.css";
 
-const VideoList = (props) => (
+const VideoList = ({ videos, onSelect, display }) => (
   <ul className={styles.videos}>
-    {props.videos.map((video) => (
-      <VideoItem video={video} key={video.id} />
+    {videos.map((video) => (
+      <VideoItem
+        video={video}
+        key={video.id}
+        onSelect={onSelect}
+        display={display}
+      />
     ))}
   </ul>
 );
