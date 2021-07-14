88ad9e9b869ed7b64f14ee36e1a0d037826980a8
Yunsu <kenatman0723@gmail.com>
2021-07-14 11:36:17 +0900

    Refactoring : Component는 멍청하게 만들어야한다 : 통신 등의 기능은 컴포넌트 안이 아니라 따로 class 만들어서 관리 Dependancy Injection(unit test 시 mock으로 테스트) & .env 사용법 : REACT_APP_

--- src/app.jsx
+++ src/app.jsx
@@ -3,39 +3,18 @@ import styles from "./app.module.css";
 import SearchHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAfKjuw7TDZWDJAZZhHb6MDzlB5KuKlb1k",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   const handleSearch = (term) => {
-    var requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=${term}&key=AIzaSyAfKjuw7TDZWDJAZZhHb6MDzlB5KuKlb1k`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      )
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+      .search(term) //
+      .then((videos) => setVideos(videos));
   };
   return (
     <div className={styles.app}>
---
+++ src/service/youtube.js
@@ -0,0 +1,36 @@
+class Youtube {
+  constructor(key) {
+    this.key = key;
+    this.getRequestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+  }
+  mostPopular = async () => {
+    try {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+        this.getRequestOptions
+      );
+      const result_1 = await response.json();
+      return result_1.items;
+    } catch (error) {
+      return console.log("error", error);
+    }
+  };
+
+  search = async (term) => {
+    try {
+      const response = await fetch(
+        `https://youtube.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=25&q=${term}&key=${this.key}`,
+        this.getRequestOptions
+      );
+      const result_1 = await response.json();
+      return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
+    } catch (error) {
+      return console.log("error", error);
+    }
+  };
+}
+
+export default Youtube;
